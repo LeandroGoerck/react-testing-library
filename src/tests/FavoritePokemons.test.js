5735451fec3e3a286eac8a446f8336cf92aae7d3
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-12-01 00:58:56 -0300

    fix userEvent wass missing

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
